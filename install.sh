7af3f5a7f2640942fa18c9fa8e5dbd25fa573436
zhixunstudio <95196152+zhixunstudio@users.noreply.github.com>
2022-04-04 15:51:01 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo add-apt-repository ppa:gladky-anton/lammps
 sudo add-apt-repository ppa:openkim/latest
 sudo apt-get update
 sudo apt-get install lammps-stable
+wget https://www.ovito.org/download/3106/ovito-basic-3.7.3-x86_64.tar.xz
