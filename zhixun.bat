44e4217f6bd65cbf21d2cec37b14b6bf97562365
Zhixun <95196152+zhixunstudio@users.noreply.github.com>
2022-09-07 17:29:36 +0800

    Add files via upload

---
+++ zhixun.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ÏÂÔØfftw-3.3.8.tar.gz
+wget http://www.fftw.org/fftw-3.3.8.tar.gz
+
+#ÏÂÔØmpich-3.3.2.tar.gz
+wget http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz
+
+#ÏÂÔØlammps-stable.tar.gz
+wget https://lammps.sandia.gov/tars/lammps-stable.tar.gz
