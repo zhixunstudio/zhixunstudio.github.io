f71834cff4cbcc494b05a471d1be10b6e5b1c8f3
Zhixun <95196152+zhixunstudio@users.noreply.github.com>
2022-09-07 19:04:06 +0800

    Update zhixun.bat

--- zhixun.bat
+++ zhixun.bat
@@ -1,9 +1,9 @@
 #!/bin/bash
-#œ¬‘ÿfftw-3.3.8.tar.gz
+#√è√Ç√î√òfftw-3.3.8.tar.gz
 wget http://www.fftw.org/fftw-3.3.8.tar.gz
 
-#œ¬‘ÿmpich-3.3.2.tar.gz
+#√è√Ç√î√òmpich-3.3.2.tar.gz
 wget http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz
 
-#œ¬‘ÿlammps-stable.tar.gz
-wget https://lammps.sandia.gov/tars/lammps-stable.tar.gz
+#√è√Ç√î√òlammps-stable.tar.gz
+wget https://lammps.sandia.gov/tars/lammps-stable.tar.gz --no-check-certificate
