fa6f0236fe80064a65f360117e267ddfc2a8dfeb
Zhixun <95196152+zhixunstudio@users.noreply.github.com>
2022-09-07 20:52:53 +0800

    Update zhixun.bat

--- zhixun.bat
+++ zhixun.bat
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-sudo su					#
 apt-get install gcc			#安装gcc
 apt-get install g++			#安装g++
 
@@ -9,11 +7,11 @@ apt-get install make			#安装make
 
 apt-get install vim                     #安装vim
 
-#ÏÂÔØfftw-3.3.8.tar.gz
+#安装fftw-3.3.8.tar.gz
 wget http://www.fftw.org/fftw-3.3.8.tar.gz
 
-#ÏÂÔØmpich-3.3.2.tar.gz
+#安装mpich-3.3.2.tar.gz
 wget http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz
 
-#ÏÂÔØlammps-stable.tar.gz
+#安装lammps-stable.tar.gz
 wget https://lammps.sandia.gov/tars/lammps-stable.tar.gz --no-check-certificate
