5f32b4aa718698553537b4a144261c618a19f5b6
Zhixun <95196152+zhixunstudio@users.noreply.github.com>
2022-09-07 19:10:34 +0800

    Update zhixun.bat

--- zhixun.bat
+++ zhixun.bat
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+sudo su					#
+apt-get install gcc			#安装gcc
+apt-get install g++			#安装g++
+
+apt-get install gfortran		#安装gfortran
+apt-get install make			#安装make
+
+apt-get install vim                     #安装vim
+
 #ÏÂÔØfftw-3.3.8.tar.gz
 wget http://www.fftw.org/fftw-3.3.8.tar.gz
 
